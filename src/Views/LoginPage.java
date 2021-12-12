cc16f099b9c95a92afafaae710af58695700593c
farhan-hafizh <farhannovyanh@gmail.com>
2021-12-12 13:49:54 +0700

    innitial commit

---
+++ src/Views/LoginPage.java
@@ -0,0 +1,76 @@
+package Views;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Group;
+
+public class LoginPage {
+
+	protected Shell shell;
+	private Text text;
+	private Text text_1;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			LoginPage window = new LoginPage();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(587, 393);
+		shell.setText("CoffeeVibes");
+		
+		Label lblUsername = new Label(shell, SWT.NONE);
+		lblUsername.setBounds(63, 67, 70, 20);
+		lblUsername.setText("Username");
+		
+		Label lblPassword = new Label(shell, SWT.NONE);
+		lblPassword.setBounds(63, 113, 70, 20);
+		lblPassword.setText("Password");
+		
+		text = new Text(shell, SWT.BORDER);
+		text.setBounds(175, 67, 154, 26);
+		
+		text_1 = new Text(shell, SWT.BORDER);
+		text_1.setBounds(175, 107, 154, 26);
+		
+		Button btnLogin = new Button(shell, SWT.NONE);
+		btnLogin.setBounds(77, 176, 90, 30);
+		btnLogin.setText("Login");
+		
+		Button btnReset = new Button(shell, SWT.NONE);
+		btnReset.setBounds(239, 176, 90, 30);
+		btnReset.setText("Reset");
+
+	}
+}
